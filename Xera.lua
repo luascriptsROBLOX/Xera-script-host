ccbfed913afd88b95ec64080adeb1fe89f4e100f
luascriptsROBLOX <borahkokil35@gmail.com>
2025-01-19 11:53:09 +0530

    Update and rename starry.lua to Xera.lua

---
+++ Xera.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/luascriptsROBLOX/Xera.lpac/refs/heads/main/Src/Loader.lua, true))()
--- starry.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://github.com/hello-n-bye/starry/blob/main/main.lua?raw=true", true))()
